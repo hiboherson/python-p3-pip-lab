1f2cf8895d1ca21c9dc139b7f8020537e5d18452
hiboherson <hiboherson@gmail.com>
2024-09-11 10:46:51 -0700

    final commit

--- lib/versions.py
+++ lib/versions.py
@@ -1,12 +1,11 @@
-import sys
-import requests
-import pytest
-
 def python_version():
+    import sys
     return sys.version_info
 
 def requests_version():
+    import requests
     return requests.__version__
 
 def pytest_version():
+    import pytest
     return pytest.__version__
